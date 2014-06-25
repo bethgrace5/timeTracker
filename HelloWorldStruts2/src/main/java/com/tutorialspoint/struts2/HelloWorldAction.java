a50d52450e7835717e4f756c6632ed2f3bd1394d
Kevin Mershon <kevin@slixbits.com>
2014-06-25 13:43:08 -0700

    Make HelloWorldAction extend ActionSupport

--- HelloWorldStruts2/src/main/java/com/tutorialspoint/struts2/HelloWorldAction.java
+++ HelloWorldStruts2/src/main/java/com/tutorialspoint/struts2/HelloWorldAction.java
@@ -1,17 +1,19 @@
 // Action class responds to a user action when a user clicks a URL
-// each URL needs an action class. 
+// each URL needs an action class.
 // usually use class name directly as action name
 
 package com.tutorialspoint.struts2;
 
-public class HelloWorldAction{
+import com.opensymphony.xwork2.ActionSupport;
+
+public class HelloWorldAction extends ActionSupport {
    private String name;
 
 // execute method is called by Struts2
    public String execute() throws Exception {
       return "success";
    }
-   
+
    public String getName() {
       return name;
    }
