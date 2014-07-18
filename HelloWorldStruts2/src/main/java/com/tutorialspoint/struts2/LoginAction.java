e8d5a505d39ea37af1969e2d1b60f1c61858b8e5
Kevin Mershon <kevin@slixbits.com>
2014-07-18 09:09:43 -0700

    Users can now login

---
+++ HelloWorldStruts2/src/main/java/com/tutorialspoint/struts2/LoginAction.java
@@ -0,0 +1,43 @@
+package com.tutorialspoint.struts2;
+
+import java.util.Map;
+
+import org.apache.struts2.interceptor.SessionAware;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class LoginAction extends ActionSupport implements SessionAware {
+    private String userName;
+    private String password;
+    private Map<String, Object> session;
+
+    public String execute() {
+        User user = Database.findUserByUsernameAndPassword(
+            this.userName, this.password);
+        if (user == null) {
+            return "error";
+        }
+        session.put("userId", user.getId());
+        return "success";
+    }
+
+    public void setSession(Map<String, Object> session) {
+        this.session = session;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
