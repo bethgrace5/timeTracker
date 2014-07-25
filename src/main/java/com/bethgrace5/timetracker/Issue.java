3096440778af322ab4e53943ff988cdce71a9a78
Kevin Mershon <kevin@slixbits.com>
2014-07-24 18:47:51 -0700

    Implement Issue class and Hibernate mapping (closes #17)

--- src/main/java/com/bethgrace5/timetracker/Issue.java
+++ src/main/java/com/bethgrace5/timetracker/Issue.java
@@ -3,14 +3,13 @@ package com.bethgrace5.timetracker;
 import java.util.HashSet;
 import java.util.Set;
 
-public class Milestone {
+public class Issue {
     private int id;
-    private String name;
+    private String title;
     private Repository repository;
+    private Milestone milestone;
 
-    private Set<Issue> issues = new HashSet<Issue>();
-
-    public Milestone() {
+    public Issue() {
         // default constructor required by Hibernate
     }
 
@@ -21,11 +20,11 @@ public class Milestone {
         this.id = id;
     }
 
-    public String getName() {
-        return this.name;
+    public String getTitle() {
+        return this.title;
     }
-    public void setName(String name) {
-        this.name = name;
+    public void setTitle(String title) {
+        this.title = title;
     }
 
     public Repository getRepository() {
@@ -35,15 +34,14 @@ public class Milestone {
         this.repository = repository;
     }
 
-    public Set<Issue> getIssues() {
-        return this.issues;
+    public Milestone getMilestone() {
+        return this.milestone;
     }
-    public void setIssues(Set<Issue> issues) {
-        this.issues = issues;
+    public void setMilestone(Milestone milestone) {
+        this.milestone = milestone;
     }
 
     public String toString() {
         return this.repository.toString();
     }
-
 }
