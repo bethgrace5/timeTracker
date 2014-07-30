ba4fac91aa2dc261802d45b880d98658976f0de3
Beth Armitage <bethgrace5@gmail.com>
2014-07-30 12:36:50 -0700

    redirects to login if no session exists

--- src/main/java/com/bethgrace5/timetracker/LoggingInterceptor.java
+++ src/main/java/com/bethgrace5/timetracker/LoggingInterceptor.java
@@ -4,27 +4,18 @@ import com.opensymphony.xwork2.ActionInvocation;
 import com.opensymphony.xwork2.interceptor.Interceptor;
 import org.apache.struts2.interceptor.SessionAware;
 import java.util.Map;
-import java.lang.NullPointerException;
 
 public class LoggingInterceptor implements Interceptor, SessionAware{
-
     private Map<String, Object> session;
+
     public String intercept(ActionInvocation invocation) throws Exception{
 
-        //try{ 
-            //System.out.println(session.get("userId"));
-            //FIXME: before testing if "userId" is null exception is thrown
-            
-            //if(session.get("userId") == null){
-            //System.out.println("inside logging interceptor");
-                //return "show-login";
-            //}
-        //}
-        //catch (NullPointerException e){
-        //}
-        System.out.println("inside logging interceptor");
+        session = invocation.getInvocationContext().getSession();
+        if(session.get("userId") == null){
+            System.out.println("Session is null");
+            return "show-login";
+        }
         String result = invocation.invoke();
-
         return result;
     }
 
